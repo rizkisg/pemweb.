8e3d0bc67eec0d793040d5846da54706e15910f8
rizkisg <152236474+rizkisg@users.noreply.github.com>
2024-09-07 20:39:06 +0700

    Add files via upload

---
+++ diajar web/script.js
@@ -0,0 +1,10 @@
+document.getElementById("profile-img").addEventListener("click", function() {
+    alert("This is Rizki's profile picture!");
+});
+
+document.getElementById("linkedin-link").addEventListener("mouseover", function() {
+    this.style.color = "#004d4d";
+});
+document.getElementById("linkedin-link").addEventListener("mouseout", function() {
+    this.style.color = "#008080";
+});
